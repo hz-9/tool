a6f7e63e6390e98be8353c9292610d4616ae718f
Chen Zhen <heavenzhen999@163.com>
2024-09-02 00:26:28 +0800

    ci: update `ignore` files and `LICENSE`

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,38 +1,38 @@
-// 配置可参考 https://prettier.io/en/configuration.html
+// Configuration reference: https://prettier.io/en/configuration.html
 
 module.exports = {
   plugins: [
     './common/autoinstallers/rush-prettier/node_modules/@trivago/prettier-plugin-sort-imports/lib/src/index.js',
   ],
 
-  // 使用较大的打印宽度，因为 Prettier 的换行设置似乎是针对没有注释的 JavaScript.
+  // Use a larger print width because Prettier's line wrapping seems to be for JavaScript without comments.
   printWidth: 120,
 
-  // 全部使用 lf 作为 行尾序列
+  // Use lf as the end of line sequence for all files.
   endOfLine: 'lf',
 
-  // 单引号代替双引号
+  // Use single quotes instead of double quotes.
   singleQuote: true,
 
-  // 对于 ES5 而言, 尾逗号不能用于函数参数，因此使用它们只能用于数组
+  // For ES5, trailing commas cannot be used for function parameters, so they can only be used for arrays.
   trailingComma: 'es5',
 
-  // 箭头函数
+  // Use parentheses around arrow function parameters always.
   arrowParens: 'always',
 
-  // 行尾分号
+  // Disable semicolons at the end of lines.
   semi: false,
 
-  // false 将 > 放在单独一行
+  // Place the closing bracket of a block on a new line.
   bracketSameLine: false,
 
-  // html 空白敏感度，需要设置为 ignore，来保证 vue 的合理化配置
+  // Set htmlWhitespaceSensitivity to ignore to ensure reasonable configuration for Vue.
   htmlWhitespaceSensitivity: 'ignore',
 
-  // 是 html vue jsx 中每行强制使用单个属性
+  // Force each attribute in html, vue, and jsx to be on a separate line.
   singleAttributePerLine: true,
 
-  // imports
+  // @trivago/prettier-plugin-sort-imports
   importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^@hz9/(.*)$', '^@hz9-(.*)$', '^@/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
